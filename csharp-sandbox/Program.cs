6741d4b344d4c001ace94d4d90f9323aad6fbd37
legokichi <legokichi@gmail.com>
2017-06-12 19:48:14 +0900

    update

--- csharp-sandbox/Program.cs
+++ csharp-sandbox/Program.cs
@@ -15,52 +15,58 @@ namespace csharpsandbox {
       //ParalellSandbox().Wait();
       //AsyncSandbox().Wait();
       AsyncPipeSandbox().Wait();
-			sw.Stop();
+      sw.Stop();
       Console.WriteLine(String.Format("{0}ms", sw.ElapsedMilliseconds));
       Console.WriteLine("finish");
     }
 
     static async Task AsyncPipeSandbox() {
       using(var ct = new CancellationTokenSource()){
-				var reader = new Task(() => {
-					Console.WriteLine("starting task");
-					var proc = new Process();
-					proc.StartInfo.FileName = @"node";
-					proc.StartInfo.Arguments = @"-e 'setInterval(()=>process.stdout.write(""""+Date.now()+""\n""),1000);'";
-					proc.StartInfo.UseShellExecute = false;
-					proc.StartInfo.RedirectStandardInput = true;
-					proc.StartInfo.RedirectStandardOutput = true;
-					proc.Start();
-					Console.WriteLine("proc started");
-					using(var br = new BinaryReader(proc.StandardOutput.BaseStream)) {
-						try {
-							while(!ct.IsCancellationRequested) {
-								var bytes = br.ReadBytes(1);
-								var str = System.Text.Encoding.ASCII.GetString(bytes);
-								Console.Write(str);
-							}
-							proc.Close();
-							ct.Token.ThrowIfCancellationRequested();
-						} catch(EndOfStreamException err) {
-							Console.WriteLine("Error writing the data.\n{0}", err.GetType().Name);
-							proc.Close();
-						}
-					}
-				});
-				reader.Start();
-				Console.WriteLine("task started");
-				await Task.Delay(10 * 1000);
-				try {
-					ct.Cancel();
-					reader.Wait();
-				} catch(AggregateException ae) {
-					foreach(var err in ae.InnerExceptions) {
-						Console.WriteLine(ae.Message + ": " + err.Message);
-					}
-				}
+        var reader = new Task(() => {
+          Console.WriteLine("starting task");
+          var proc = new Process();
+          proc.StartInfo.FileName = @"node";
+          proc.StartInfo.Arguments = @"-e 'setInterval(()=>process.stdout.write(""""+Date.now()+""\\n""),10);'";
+          proc.StartInfo.UseShellExecute = false;
+          proc.StartInfo.RedirectStandardInput = true;
+          proc.StartInfo.RedirectStandardOutput = true;
+          proc.StartInfo.RedirectStandardError = true;
+          proc.Start();
+          Console.WriteLine("proc started");
+          using(var br = new BinaryReader(proc.StandardOutput.BaseStream)) {
+            try {
+              while(!ct.IsCancellationRequested) {
+                var bytes = br.ReadBytes(1);
+                var str = System.Text.Encoding.ASCII.GetString(bytes);
+                Console.Write(str);
+              }
+            } catch(EndOfStreamException err) {
+              Console.WriteLine("Error writing the data.");
+              Console.WriteLine("{0}: {1}: {2}", err.GetType().Name, err.Message);
+              proc.Close();
+              return;
+            }
+          }
+                    proc.Close();
+                    ct.Token.ThrowIfCancellationRequested();
+        });
+
+        reader.Start();
+        Console.WriteLine("task started");
+        await Task.Delay(1 * 1000);
+        try {
+          ct.Cancel();
+          Console.WriteLine("cancel called");
+          reader.Wait();
+          Console.WriteLine("reader done");
+        } catch(AggregateException ae) {
+          foreach(var err in ae.InnerExceptions) {
+            Console.WriteLine("{0}: {1}: {2}", ae.Message, err.GetType().Name, err.Message);
+          }
+        }
       }
-			Console.WriteLine("Done!");
-			return;
+      Console.WriteLine("Done!");
+      return;
     }
 
     static async Task AsyncFIFOSandbox() {
@@ -118,17 +124,17 @@ namespace csharpsandbox {
     }
 
     static async Task AsyncSandbox(){
-			Console.WriteLine("Promise.all test");
-			await Task.WhenAll(
-			  ((Func<Task>)(async () => {
-				  await Task.Delay(100);
-				  Console.WriteLine("100");
-			  }))(),
-			  ((Func<Task>)(async () => {
-				  await Task.Delay(200);
-				  Console.WriteLine("200");
-			  }))()
-			);
+      Console.WriteLine("Promise.all test");
+      await Task.WhenAll(
+        ((Func<Task>)(async () => {
+          await Task.Delay(100);
+          Console.WriteLine("100");
+        }))(),
+        ((Func<Task>)(async () => {
+          await Task.Delay(200);
+          Console.WriteLine("200");
+        }))()
+      );
       return;
     }
 
@@ -145,9 +151,9 @@ namespace csharpsandbox {
 
       var lst = new List<string>(arr);
 
-			Parallel.ForEach(lst, (line) => {
-				Console.WriteLine(line);
-			});
+      Parallel.ForEach(lst, (line) => {
+        Console.WriteLine(line);
+      });
     }
 
     static Task WaitForExitAsync(this Process proc, CancellationToken ct = default(CancellationToken)) {
